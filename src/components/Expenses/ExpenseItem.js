32ebb9948c49608b173977da6cf6656fc69604ac
Sahil <sahil.suvarna786@gmail.com>
2021-12-21 16:36:53 +0530

    submitform

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.97;
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">₹{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
